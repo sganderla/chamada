687cc31ff0c6dd541edfbe9f2f694c4e3a227f55
Eduardo Sganderla <eduardo.sganderla@softplan.com.br>
2023-02-27 09:01:14 -0300

    Commit inicial

---
+++ src/test/java/br/com/uniamerica/chamada/ChamadaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uniamerica.chamada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
